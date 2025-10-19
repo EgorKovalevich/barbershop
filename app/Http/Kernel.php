3daaca2b5e680a39b09e22a8efae95ad1a3e9f37
EgorKovalevich <91996191+EgorKovalevich@users.noreply.github.com>
2025-10-20 02:22:11 +0300

    Revert "Add client appointment dashboard and editing flow"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
-        'client' => \App\Http\Middleware\EnsureUserIsClient::class,
     ];
 }
